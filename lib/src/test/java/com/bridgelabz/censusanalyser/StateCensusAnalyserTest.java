fc82e07fb514c34d2d10999d89e70bc0f5056592
Akshaya HP <akshayhulkuli@gmail.com>
2021-09-22 00:37:30 +0530

    ADDED: junit test method to test loading csv data

---
+++ lib/src/test/java/com/bridgelabz/censusanalyser/StateCensusAnalyserTest.java
@@ -0,0 +1,22 @@
+package com.bridgelabz.censusanalyser;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import com.bridgelabz.censusanalyser.StateCensusAnalyser;
+public class StateCensusAnalyserTest {
+	private static final String CORRECT_CSV_FILE_PATH = "resources/IndianStateCensusData.csv";
+	private static final String WRONG_CSV_FILE_PATH = "resources/test/IndianStateCensusData.csv";
+	private static final String INCORRECT_FILE_FORMAT = "resources/IncorrectFileFormat.txt";
+	private static final String CSV_WITH_WRONG_DELIMITER = "./src/test/resources/CensusDataWithWrongDelimiter.csv";
+	private static final String CSV_WITH_INCORRECT_HEADER = "./src/test/resources/CensusDataIncorrectHeader.csv";
+	@Test
+	public void givenIndianCensusCSVFile_WhenCorrect_ReturnsNumberOfRows() {
+	
+		StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser();
+		int rows = stateCensusAnalyser.loadIndianCensusData(CORRECT_CSV_FILE_PATH);
+		Assert.assertEquals(29,rows);
+	
+	}
+	
+}
